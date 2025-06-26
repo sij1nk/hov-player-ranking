5ac0015812a1944fe016e669becad7a7478d227d
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-06-26 22:56:23 +0200

    Get top1 player's name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-function main() {
-  console.log("Hello world!");
+import fetch from "node-fetch";
+import { parse } from "node-html-parser";
+import process from "node:process";
+
+const result = await fetch(
+  "https://steamcommunity.com/stats/2504090/leaderboards/16808192?sr=1"
+);
+const text = await result.text();
+const root = parse(text);
+const stats = root.querySelector("#stats");
+if (!stats) {
+  console.error("no stats");
+  process.exit(1);
 }
 
-main();
+const first = stats.querySelectorAll(".lbentry")[0];
+const pname = first.querySelector(".playerName")?.innerText;
+console.log(pname);
