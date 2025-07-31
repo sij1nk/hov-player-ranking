1437b1b29e968de0086feadeb664aabf4a1cb758
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-07-31 08:22:23 +0200

    Migrate old snapshots to local DB

--- pkg/migrate-old-snapshots/migrate-old-snapshots.sh
+++ pkg/migrate-old-snapshots/migrate-old-snapshots.sh
@@ -5,12 +5,13 @@ shas=$(git log --all --oneline --grep="^Update$" | cut -d ' ' -f 1)
 mkdir -p /tmp/hov-commits
 
 for sha in $shas; do
-  echo "$sha"
+  echo "Migrating $sha"...
   filename="/tmp/hov-commits/$sha.md"
   git show "${sha}:README.md" | sed -n '/| Rank/,$p' >"$filename"
-  npx mdt2json -f "$filename" -l AoS --include-html -k "$filename.out"
-  sed -n 's/```//;/\[/,$p' "$filename.out" | npm run migrate-single-snapshot
-  exit 1
+  npx mdt2json -f "$filename" -l AoS --include-html -k "$filename.out" >/dev/null
+  date=$(git show "$sha" --summary --format="%ad")
+  table=$(sed -n 's/```//;/\[/,$p' "$filename.out")
+  printf "%s\n%s" "$date" "$table" | npm run migrate-single-snapshot
 done
 
 rm -rf /tmp/hov-commits
--- pkg/migrate-old-snapshots/src/index.ts
+++ pkg/migrate-old-snapshots/src/index.ts
@@ -1,6 +1,7 @@
 import process from "node:process";
-import type { PlayerJson } from "./types.ts";
+import type { Player, PlayerJson } from "./types.ts";
 import { playerJsonToPlayer } from "./convert.ts";
+import { PrismaClient } from "../../data/src/generated/prisma/index.js";
 
 async function readStdin(): Promise<string> {
   return new Promise((res) => {
@@ -21,7 +22,61 @@ async function readStdin(): Promise<string> {
   });
 }
 
-const json = await readStdin();
+async function writeToDb(date: string, players: Player[]): Promise<void> {
+  const prisma = new PrismaClient();
+
+  await prisma.$transaction(async (tx) => {
+    const newLeaderboardSnapshot = await tx.leaderboardSnapshot.create({
+      data: {
+        date,
+      },
+    });
+
+    const upsertPlayerPromises = players.map((p) => {
+      const newStats = {
+        snapshotId: newLeaderboardSnapshot.id,
+        totalRank: p.totalRank,
+        totalScore: p.totalScore,
+        pvpRank: p.pvpRank,
+        pvpScore: p.pvpScore,
+        scoreRatioMin: p.scoreRatioMin,
+        scoreRatioMax: p.scoreRatioMax,
+      };
+
+      return tx.player.upsert({
+        where: {
+          steamId: p.steamId,
+          steamIdType: p.steamIdType,
+        },
+        update: {
+          name: p.name,
+          stats: {
+            create: newStats,
+          },
+        },
+        create: {
+          name: p.name,
+          steamId: p.steamId,
+          steamIdType: p.steamIdType,
+          stats: {
+            create: newStats,
+          },
+        },
+        include: {
+          stats: true,
+        },
+      });
+    });
+
+    await Promise.all(upsertPlayerPromises);
+  });
+}
+
+const input = await readStdin();
+const lines = input.split("\n");
+const date = new Date(lines[0]).toISOString();
+const json = lines.slice(1).join("\n");
 const jsonPlayers: PlayerJson[] = JSON.parse(json);
 const players = jsonPlayers.map(playerJsonToPlayer);
-console.log(players);
+
+await writeToDb(date, players);
