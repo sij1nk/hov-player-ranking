de10daabd955864a1572852bc3e972b154ec71b3
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-07-31 06:50:27 +0200

    Convert from PlayerJson to Player & reformat

--- pkg/migrate-old-snapshots/src/index.ts
+++ pkg/migrate-old-snapshots/src/index.ts
@@ -1,56 +1,27 @@
 import process from "node:process";
-
-type PlayerJson = {
-	Rank: string;
-	Player: string;
-	["Total rank"]: string;
-	["Pvp rank"]: string;
-	["Total score"]: string;
-	["Pvp score"]: string;
-	["Score ratio"]: string;
-};
-
-enum SteamIdType {
-	ID = "ID",
-	CUSTOM = "CUSTOM",
-}
-
-type Player = {
-	rank: number;
-	steamId: string;
-	steamIdType: SteamIdType;
-	name: string;
-	stats: PlayerStats;
-};
-
-type PlayerStats = {
-	totalRank?: number;
-	totalScore?: number;
-	pvpRank?: number;
-	pvpScore?: number;
-	scoreRatioMin: number;
-	scoreRatioMax: number;
-};
+import type { PlayerJson } from "./types.ts";
+import { playerJsonToPlayer } from "./convert.ts";
 
 async function readStdin(): Promise<string> {
-	return new Promise((res) => {
-		const stdin = process.stdin;
-		const chunks: string[] = [];
+  return new Promise((res) => {
+    const stdin = process.stdin;
+    const chunks: string[] = [];
 
-		stdin.on("readable", () => {
-			let chunk;
-			while (null !== (chunk = stdin.read())) {
-				chunks.push(chunk);
-			}
-		});
+    stdin.on("readable", () => {
+      let chunk;
+      while (null !== (chunk = stdin.read())) {
+        chunks.push(chunk);
+      }
+    });
 
-		stdin.on("end", () => {
-			const content = chunks.join("");
-			res(content);
-		});
-	});
+    stdin.on("end", () => {
+      const content = chunks.join("");
+      res(content);
+    });
+  });
 }
 
 const json = await readStdin();
-const players: PlayerJson[] = JSON.parse(json);
+const jsonPlayers: PlayerJson[] = JSON.parse(json);
+const players = jsonPlayers.map(playerJsonToPlayer);
 console.log(players);
