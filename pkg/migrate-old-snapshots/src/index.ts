fabd3e16d38ca4671ad9d850b81e077e68767d74
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-08-03 16:47:01 +0200

    Speed up migration (~2x)

--- pkg/migrate-old-snapshots/src/index.ts
+++ pkg/migrate-old-snapshots/src/index.ts
@@ -1,26 +1,10 @@
 import process from "node:process";
-import type { Player, PlayerJson } from "./types.ts";
+import { MarkdownTable2Json, JsonLayout } from "mdt2json";
+import { promisify } from "node:util";
+import { exec as _exec } from "node:child_process";
+import type { Player } from "./types.ts";
 import { playerJsonToPlayer } from "./convert.ts";
 import { PrismaClient } from "../../data/src/generated/prisma/index.js";
-
-async function readStdin(): Promise<string> {
-  return new Promise((res) => {
-    const stdin = process.stdin;
-    const chunks: string[] = [];
-
-    stdin.on("readable", () => {
-      let chunk;
-      while (null !== (chunk = stdin.read())) {
-        chunks.push(chunk);
-      }
-    });
-
-    stdin.on("end", () => {
-      const content = chunks.join("");
-      res(content);
-    });
-  });
-}
 
 async function writeToDb(date: string, players: Player[]): Promise<void> {
   const prisma = new PrismaClient();
@@ -72,11 +56,49 @@ async function writeToDb(date: string, players: Player[]): Promise<void> {
   });
 }
 
-const input = await readStdin();
-const lines = input.split("\n");
-const date = new Date(lines[0]).toISOString();
-const json = lines.slice(1).join("\n");
-const jsonPlayers: PlayerJson[] = JSON.parse(json);
-const players = jsonPlayers.map(playerJsonToPlayer);
+function arg(i: number): number | undefined {
+  const s = process.argv[i];
+  if (s) return Number(s);
+  return undefined;
+}
 
-await writeToDb(date, players);
+const exec = promisify(_exec);
+
+const from = arg(2) ?? 0;
+const count = arg(3);
+
+const shasResult = await exec(`git log --all --oneline --grep="^Update$"`);
+if (shasResult.stderr) console.error("Error: " + shasResult.stderr);
+const shas = shasResult.stdout.split("\n").map((l) => l.split(" ")[0]);
+
+const to = count ? from + count : shas.length;
+
+const currentShas = shas.slice(from, to);
+
+let i = from;
+for (const sha of currentShas) {
+  console.log(`Processing (${i}/${to})...`);
+  i += 1;
+
+  const readmeResult = await exec(`git show ${sha}:README.md`);
+  const dateResult = await exec(`git show ${sha} --summary --format=%ad`);
+
+  if (readmeResult.stderr) console.error("Error: " + readmeResult.stderr);
+  const readme = readmeResult.stdout;
+  const date = new Date(dateResult.stdout.split("\n")[0]).toISOString();
+
+  const mdTable = readme.split("<br/>")[1];
+
+  const convert = new MarkdownTable2Json({
+    markdownString: mdTable,
+    includeHtml: true,
+    layout: JsonLayout.AoS,
+    minify: false,
+  });
+
+  const mdJsonString = convert.transform();
+  const jsonString = mdJsonString.replaceAll(/```.*/g, "");
+  const json = JSON.parse(jsonString);
+  const players = json.map(playerJsonToPlayer);
+  await writeToDb(date, players);
+}
