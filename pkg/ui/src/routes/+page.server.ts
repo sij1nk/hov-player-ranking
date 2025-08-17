b42ae526a7c5421c0b2f07798a63ccdf42474276
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-08-17 11:03:15 +0200

    Delete unused page and layout + use latest snapshot id as route param

--- pkg/ui/src/routes/+page.server.ts
+++ pkg/ui/src/routes/+page.server.ts
@@ -1,6 +1,11 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = () => {
-  redirect(307, "/leaderboard/bar");
+export const load: PageServerLoad = async ({ parent }) => {
+  const { snapshots } = await parent();
+
+  // TODO: sort by newest first, get [0]
+  const latestSnapshot = snapshots.reduce((prev, curr) => (prev.date > curr.date ? prev : curr));
+
+  redirect(307, `/leaderboard/${latestSnapshot.id}`);
 };
