e7963810751bef1e0a552129edaee061e1e345b7
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-08-20 10:29:18 +0200

    Select date from calendar dialog

--- pkg/ui/src/lib/components/snapshot-picker-dialog/snapshot-picker-dialog.svelte
+++ pkg/ui/src/lib/components/snapshot-picker-dialog/snapshot-picker-dialog.svelte
@@ -4,7 +4,12 @@
   import Separator from "../ui/separator/separator.svelte";
   import Calendar from "../ui/calendar/calendar.svelte";
   import { CalendarDate } from "@internationalized/date";
-  import { toCalendarDate } from "$lib/date";
+  import { equalsCalendarDate, toCalendarDate } from "$lib/date";
+  import Button from "../ui/button/button.svelte";
+  import Table2 from "@lucide/svelte/icons/table-2";
+  import ScrollArea from "../ui/scroll-area/scroll-area.svelte";
+  import { goto } from "$app/navigation";
+  import { buttonVariants } from "../ui/button";
 
   type Props = {
     currentSnapshot: Snapshot;
@@ -15,11 +20,24 @@
   let { currentSnapshot, snapshots, snapshotCalendarDates }: Props = $props();
   let isLatest = $derived(snapshots[0].id === currentSnapshot.id);
 
-  let currentCalendarDate = $state<CalendarDate>(toCalendarDate(currentSnapshot.date));
+  let isOpen = $state(false);
+
+  let currentCalendarDate = $derived<CalendarDate>(toCalendarDate(currentSnapshot.date));
+
+  let matchingSnapshots = $derived(
+    snapshots
+      .filter((s) => equalsCalendarDate(s.date, currentCalendarDate))
+      .sort((left, right) => left.date.valueOf() - right.date.valueOf()),
+  );
+
+  let selectSnapshot = (snapshot: Snapshot) => {
+    isOpen = false;
+    goto(snapshot.dateShort);
+  };
 </script>
 
-<Dialog.Root>
-  <Dialog.Trigger>
+<Dialog.Root bind:open={isOpen}>
+  <Dialog.Trigger class={buttonVariants({ variant: "ghost" })}>
     <div class="dialog-trigger">
       <span>{currentSnapshot.date.toLocaleString()}</span>
       {#if isLatest}
@@ -30,13 +48,31 @@
   <Dialog.Content>
     <Dialog.Header>
       <Dialog.Title>Pick a leaderboard snapshot</Dialog.Title>
-      <Separator />
+    </Dialog.Header>
+    <Separator />
+    <div class="dialog-content">
       <Calendar
         type="single"
         bind:value={currentCalendarDate}
         isDateDisabled={(date) => snapshotCalendarDates.every((s) => s.compare(date) !== 0)}
       />
-    </Dialog.Header>
+      <ScrollArea class="h-120">
+        <ul>
+          {#each matchingSnapshots as matchingSnapshot (matchingSnapshot.id)}
+            <li>
+              <Button
+                href={matchingSnapshot.dateShort}
+                variant="ghost"
+                onclick={() => selectSnapshot(matchingSnapshot)}
+              >
+                <Table2 />
+                {matchingSnapshot.date.toLocaleString()}</Button
+              >
+            </li>
+          {/each}
+        </ul></ScrollArea
+      >
+    </div>
   </Dialog.Content>
 </Dialog.Root>
 
@@ -45,5 +81,10 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  .dialog-content {
+    display: flex;
+    padding: 1em;
   }
 </style>
--- pkg/ui/src/lib/date.ts
+++ pkg/ui/src/lib/date.ts
@@ -2,3 +2,8 @@ import { CalendarDate } from "@internationalized/date";
 
 export const toCalendarDate = (date: Date): CalendarDate =>
   new CalendarDate(date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate());
+
+export const equalsCalendarDate = (date: Date, calendarDate: CalendarDate): boolean =>
+  date.getUTCFullYear() === calendarDate.year &&
+  date.getUTCMonth() + 1 === calendarDate.month &&
+  date.getUTCDate() === calendarDate.day;
