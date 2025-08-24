2b9afdf3b6d32ae69f95ae649f28a51337c2ccea
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-08-24 20:38:35 +0200

    wip

--- pkg/ui/src/lib/server/database.ts
+++ pkg/ui/src/lib/server/database.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "$lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { DATABASE_URL } from "$env/static/private";
 
-const client = new PrismaClient({
-  datasourceUrl: DATABASE_URL,
-});
+const adapter = new PrismaPg({ connectionString: DATABASE_URL });
+const client = new PrismaClient({ adapter });
 
 export { client };
--- pkg/ui/vite.config.ts
+++ pkg/ui/vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit(), wasm()],
   test: {
     expect: { requireAssertions: true },
     projects: [
